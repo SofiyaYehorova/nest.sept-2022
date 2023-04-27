ed86d677d3339f9b53dc36924365dc4acde13188
SofiyaYehorova <yehorova66@gmail.com>
2023-04-27 13:31:14 +0300

    lesson 2 part1 pets aren't done

--- src/core/configs/config.ts
+++ src/core/configs/config.ts
@@ -1,8 +1,10 @@
 import { config } from 'dotenv';
+import * as process from 'process';
 
 config();
 
 export const configs = {
   PUBLIC_PICTURE_USERS: process.env.PUBLIC_PICTURE_USERS,
   PUBLIC_PICTURE_ANIMALS: process.env.PUBLIC_PICTURE_ANIMALS,
+  BASE_URL: process.env.BASE_URL,
 };
